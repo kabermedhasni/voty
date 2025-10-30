ed4ad04bbcb8345e38d9940873a5ef16acb6a14a
kabermedhasni <kabermedhasni653@gmail.com>
2025-10-30 23:45:36 +0000

    database cleanup

--- voty.sql
+++ voty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 30, 2025 at 11:22 PM
+-- Generation Time: Oct 30, 2025 at 11:44 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -43,23 +43,6 @@ CREATE TABLE `candidates` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `candidates`
---
-
-INSERT INTO `candidates` (`id`, `name`, `ar_name`, `photo_path`, `fr_description`, `en_description`, `ar_description`, `nominated_at`, `Supporting_party`, `path_supporting_party_logo`, `id_position`, `created_by`, `created_at`) VALUES
-(19, 'Ahmed', 'أحمد', 'assets\\images\\candidates\\Ahmed_68faa510007bf_1761256720.png', 'I am a second-year student at SupNum, specializing in computer science and digital technologies. Passionate about software development, web technologies, and innovation, I am constantly looking to improve my technical and problem-solving skills through real-world projects and hackathons. I enjoy working in teams, exploring new technologies, and developing creative solutions that make a positive impact.', 'I am a second-year student at SupNum, specializing in computer science and digital technologies. Passionate about software development, web technologies, and innovation, I am constantly looking to improve my technical and problem-solving skills through real-world projects and hackathons. I enjoy working in teams, exploring new technologies, and developing creative solutions that make a positive impact.', 'أنا طالب في السنة الثانية في SupNum، أختص في علوم الحاسوب والتقنيات الرقمية. شغوف بتطوير البرمجيات والتقنيات الحديثة والابتكار، وأسعى باستمرار إلى تنمية مهاراتي التقنية والتحليلية من خلال المشاريع العملية والمشاركة في الهاكاثونات. أحب العمل الجماعي، واستكشاف التقنيات الجديدة، وتطوير حلول إبداعية تسهم في تحسين الواقع الرقمي.', '2025-10-23', 'UNEM', 'assets\\images\\supportings\\Ahmed_68faa51001d1b_1761256720.jpg', 30, NULL, '2025-10-27 08:01:56'),
-(21, 'jack', 'تلتبيس', 'assets/images/candidates/profile/candidate-placeholder.png', 'boon', 'goood', 'تبنيتش', NULL, 'Ungem', 'assets/images/candidates/party/party-placeholder.jpg', 28, 2, '2025-10-27 19:37:57'),
-(22, 'kaber', 'بتين', 'assets/images/candidates/profile/candidate-placeholder.png', 'jdkfj', 'fjdklj', ' jfkdj ', NULL, 'Undke', 'assets/images/candidates/party/party-placeholder.jpg', 28, 2, '2025-10-27 19:38:26'),
-(23, 'fatass', 'تبين', 'assets/images/candidates/profile/candidate-placeholder.png', 'bon et male', 'good and bad', 'بتميسن', NULL, 'U ke', 'assets/images/candidates/party/party-placeholder.jpg', 29, 2, '2025-10-27 19:39:40'),
-(24, 'micheal', 'تبيسمت', 'assets/images/candidates/profile/candidate-placeholder.png', 'jklj', 'fjdksjl', 'بتيم', NULL, 'fdj', 'assets/images/candidates/party/party-placeholder.jpg', 29, 2, '2025-10-27 19:40:12'),
-(25, 'jackson', 'تبينتش', 'assets/images/candidates/profile/candidate-placeholder.png', 'jfdlaj', 'jdkfj', 'تيمنتب', NULL, 'jfdls', 'assets/images/candidates/party/party-placeholder.jpg', 29, 2, '2025-10-27 19:40:46'),
-(26, 'fjdk', 'تبيم', 'assets/images/candidates/profile/candidate-placeholder.png', 'jkfld', 'jfdkls', 'بتينم', NULL, 'jfdlk', 'assets/images/candidates/party/party-placeholder.jpg', 30, 2, '2025-10-27 19:41:41'),
-(27, 'kaber', 'نائب الرئيس', 'assets/images/candidates/profile/candidate-placeholder.png', 'dfghjkl;kjhgfdsfghjkl;kjhtrertfhyukl;lkjhgfhjkl', 'dfghjkl;kjhgfdsfghjkl;kjhtrertfhyukl;lkjhgfhjkl', 'dfghjkl;kjhgfdsfghjkl;kjhtrertfhyukl;lkjhgfhjkl', NULL, 'Undke', 'assets/images/candidates/party/party-placeholder.jpg', 28, NULL, '2025-10-29 16:50:22'),
-(29, 'kaber', 'sidi', 'assets/images/candidates/profile/candidate-placeholder.png', 'ca c\'est bon', 'his the real nigga', 'تنمبتيسم', NULL, 'UGEM', 'assets/images/candidates/party/party-placeholder.jpg', 43, NULL, '2025-10-30 12:33:11'),
-(30, 'zein albidine', 'the man', 'assets/images/candidates/profile/candidate-placeholder.png', 'c\'est pas bon', 'the second nigga', 'بتينت', NULL, 'UNEM', 'assets/images/candidates/party/party-placeholder.jpg', 43, NULL, '2025-10-30 12:34:50'),
-(34, 'medn', 'dfdkl', 'assets/images/candidates/profile/candidate-placeholder.png', 'c\'e nigga', 'fuck niggas', 'تبيمتي', NULL, 'SNIM', 'assets/images/candidates/party/party-placeholder.jpg', 43, NULL, '2025-10-30 20:28:16');
-
 -- --------------------------------------------------------
 
 --
@@ -81,19 +64,6 @@ CREATE TABLE `election` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `election`
---
-
-INSERT INTO `election` (`id`, `en_organizer`, `fr_organizer`, `ar_organizer`, `election_type`, `year`, `start_date`, `end_date`, `results`, `status`, `created_by`, `created_at`) VALUES
-(15, 'SupNum', 'SupNum', 'SupNum', 'municipal', '2025', '2025-10-24', '2025-10-30', 'publish', 1, NULL, '2025-10-27 08:15:33'),
-(17, 'dfghjk', 'jack', 'jfl', 'university', '2025', '2025-12-20', '2026-12-30', 'nopublish', 1, NULL, '2025-10-27 08:15:33'),
-(22, 'jfkdlj', 'jkdlj', 'ابيتا', 'university', '2025', '2000-12-12', '2054-12-12', 'publish', 1, 1, '2025-10-27 14:17:53'),
-(23, 'jflj', 'fjdkl', 'تبنيست', 'municipal', '2002', '2000-12-12', '2028-02-02', 'nopublish', 1, 1, '2025-10-27 15:12:56'),
-(24, 'jflj', 'fjdkl', 'تبنيست', 'municipal', '2002', '2000-12-12', '2028-02-02', 'nopublish', 1, 1, '2025-10-27 15:13:00'),
-(25, 'fdjkla', 'dfjklaj', 'تبيست j', 'university', '2004', '2000-12-12', '2025-12-12', 'nopublish', 1, 1, '2025-10-27 15:18:22'),
-(26, 'UN', 'UN', 'نشلاشثق', 'governmental', '2027', '2025-10-12', '2027-12-12', 'publish', 1, 1, '2025-10-30 09:01:56');
-
 -- --------------------------------------------------------
 
 --
@@ -106,22 +76,6 @@ CREATE TABLE `election_admins` (
   `election_id` int NOT NULL,
   `assigned_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `election_admins`
---
-
-INSERT INTO `election_admins` (`id`, `admin_user_id`, `election_id`, `assigned_at`) VALUES
-(34, 2, 24, '2025-10-27 15:52:00'),
-(36, 2, 22, '2025-10-27 15:52:13'),
-(37, 2, 17, '2025-10-27 15:52:19'),
-(42, 2, 15, '2025-10-27 17:01:57'),
-(44, 2, 23, '2025-10-27 19:23:46'),
-(58, 6, 25, '2025-10-30 19:44:45'),
-(59, 2, 25, '2025-10-30 19:44:45'),
-(60, 31, 25, '2025-10-30 19:44:45'),
-(73, 6, 26, '2025-10-30 21:17:13'),
-(74, 31, 26, '2025-10-30 21:17:13');
 
 -- --------------------------------------------------------
 
@@ -139,33 +93,6 @@ CREATE TABLE `position` (
   `created_by` int DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `position`
---
-
-INSERT INTO `position` (`id`, `fr_name`, `en_name`, `ar_name`, `status`, `id_election`, `created_by`, `created_at`) VALUES
-(17, 'Président', 'President', 'الرئيس', 1, NULL, NULL, '2025-10-27 08:15:33'),
-(18, 'Vice-Président', 'Vice President', 'نائب الرئيس', 1, NULL, NULL, '2025-10-27 08:15:33'),
-(19, 'Secrétaire général', 'General Secretary', 'الكاتب العام', 1, NULL, NULL, '2025-10-27 08:15:33'),
-(24, 'Nigger', 'Nigga', 'تبتسيم', 1, NULL, 1, '2025-10-27 15:20:00'),
-(25, 'Président', 'President', 'الرئيس', 1, 25, 1, '2025-10-27 15:51:54'),
-(26, 'Président', 'President', 'الرئيس', 1, 24, 1, '2025-10-27 15:52:00'),
-(27, 'Secrétaire général', 'General Secretary', 'الكاتب العام', 1, 23, 1, '2025-10-27 15:52:07'),
-(28, 'Vice-Président', 'Vice President', 'نائب الرئيس', 1, 22, 1, '2025-10-27 15:52:13'),
-(29, 'Président', 'President', 'الرئيس', 1, 17, 1, '2025-10-27 15:52:19'),
-(30, 'Président', 'President', 'الرئيس', 1, 15, 1, '2025-10-27 15:52:28'),
-(33, 'rabit', 'fuckass', 'تنتبي', 1, NULL, 1, '2025-10-27 16:04:40'),
-(34, 'gfhjkl', 'fghjkl', 'fghjk', 1, 25, 5, '2025-10-30 02:10:13'),
-(35, 'ghjk', 'ghjkfghjk', 'fghj', 1, 25, 5, '2025-10-30 02:11:48'),
-(38, 'dfghjk', 'er1ert1hj', 'tyui', 1, 25, 5, '2025-10-30 02:13:52'),
-(39, 'dfghjk', 'dsfghj', 'ghjk', 1, 25, 5, '2025-10-30 02:14:22'),
-(41, 'fghjkltyuky', 'hjkjhk', 'ghjkhgfyguhjlhgfj', 1, 25, 5, '2025-10-30 02:17:54'),
-(42, 'dfghjdfgh', 'dfghjk', 'ghjkhgf', 1, 25, 5, '2025-10-30 02:20:23'),
-(43, 'the boy', 'the nigga', 'the man', 1, 26, 2, '2025-10-30 10:51:10'),
-(44, 'talk', 'jfdl', 'dfdkl', 1, 26, 2, '2025-10-30 10:51:30'),
-(45, 'the asshole', 'th boy', 'the man', 1, 26, 2, '2025-10-30 10:51:49'),
-(46, 'nouveau position', 'a new position', 'ابيتام', 1, 26, 6, '2025-10-30 12:58:23');
 
 -- --------------------------------------------------------
 
@@ -190,17 +117,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `user_id_hmac`, `username`, `nationality_hmac`, `email_encrypted`, `role`, `password_hash`, `created_at`) VALUES
 (1, '6P0tcPVynCO8AMTLWkPn+jqP8wFuaFJUquPPHKICqCM=', 'Kaber Sidi', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'super_admin', '$2y$10$DDPXzcf0fHCM2.bw1rs6ruoBz3Qw0bmmSmWfMvPOEtYXkRqoS3PiC', '2025-10-09 20:11:43'),
-(2, 'AcYqkgbFqszGYv7Sx1QfpI/7U9Pc1KFByDCU86gx4x0=', 'Ayman', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'admin', '$2y$10$S01d.vIaX.mp16C4G2MsV.Z/SBb7dcBPbChyga2TiwRCXRx29q92e', '2025-10-09 20:11:44'),
-(3, 'F7nxTRhdnCmbxlw1SVh0mFEvMrmBZ8WVYHMUTwAXQl0=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', '$2y$10$ARNDP3ZBx3Rb8KXYBDte5uxmBj6UCKrWJlJmurgfg2ki0uDKLBIcu', '2025-10-09 20:11:44'),
-(4, '7Ta5LD9Y8NPlE2/z+kOQzR7JYQX7AiwZ5yi8zSf+nYA=', 'Zein El abidine', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'super_admin', '$2y$10$9WXiTQx9DFjL78OqNW.sN.lYcuwVy4Ab8eJ/GAl9Ps3zD.fhxyyDa', '2025-10-09 20:11:44'),
-(5, '2nQiqlJzFg5VBPKwHXbuTc+RlRSnoZ0hyHg684OlZl0=', 'Zein El abidine Admin', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'admin', '$2y$10$k6DrfbhOHbLyToqY2Isbs.ccqlQzo1wA9h704xy0XUAUVRj82ELXa', '2025-10-09 20:11:44'),
-(6, 'brZuLvzpkm8Qg/kjRQfTfF/YmK6Kb5uxzDKN0etWyJ4=', 'amadoba', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'admin', '$2y$10$m2R5sn7xyMYMQvge6//mKul/QjimKLKc1Qu5UZoxiGCk1onbJtB9W', '2025-10-09 20:11:44'),
-(8, 'erFjToDlo6CBDjwpnPlMohrDByip5E4e53FBIHpmSCA=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', NULL, '2025-10-09 20:11:44'),
-(9, 'AwZe0UZp/3yHdpd9Dthb/n8xxUA3hc9AgKlhp56b5LE=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', NULL, '2025-10-09 20:11:44'),
-(10, 'LND3xaNN/F7+3wftJmjEHP8zB1/7TM0opnqpeuDPCQ8=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', NULL, '2025-10-09 20:11:44'),
-(22, 'yx2I3UuuaRYsDz0jgXL9XCJqM8FoyRpLW3NMiwEF6JE=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', '$2y$10$tpf3kdQRuTZzZI4W6hWPZOCEpgYXfegg6icSIMG/N5ARXTp4g.MFm', '2025-10-29 22:52:36'),
-(23, 'lpxD1+TTLCK5cz4MmYbPfLoo93sWDDGcEPf2HWF7xL0=', NULL, '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'user', NULL, '2025-10-29 22:52:36'),
-(31, 'pSekXMwq5Qmb2/ZtcmARyQ2DEKC8lKO9wEYkBiCTdzE=', 'sidina', NULL, NULL, 'admin', '$2y$10$zY2P9cu8DI5KIRMcl7Pt7.n8k4qHwxInj.EfE2XtbmI6tzWYWlwLW', '2025-10-30 12:16:23');
+(2, '7Ta5LD9Y8NPlE2/z+kOQzR7JYQX7AiwZ5yi8zSf+nYA=', 'Zein El abidine', '4OwI6ALr0zOtLdUPP0hnUyJ9s0NlH8/BCvGcZkaUcgw=', NULL, 'super_admin', '$2y$10$9WXiTQx9DFjL78OqNW.sN.lYcuwVy4Ab8eJ/GAl9Ps3zD.fhxyyDa', '2025-10-09 20:11:44');
 
 -- --------------------------------------------------------
 
@@ -213,15 +130,6 @@ CREATE TABLE `users_election` (
   `user_id_hmac` varchar(64) DEFAULT NULL,
   `id_election` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `users_election`
---
-
-INSERT INTO `users_election` (`id`, `user_id_hmac`, `id_election`) VALUES
-(1, 'yx2I3UuuaRYsDz0jgXL9XCJqM8FoyRpLW3NMiwEF6JE=', 25),
-(2, 'yx2I3UuuaRYsDz0jgXL9XCJqM8FoyRpLW3NMiwEF6JE=', 25),
-(5, 'F7nxTRhdnCmbxlw1SVh0mFEvMrmBZ8WVYHMUTwAXQl0=', 26);
 
 --
 -- Indexes for dumped tables
@@ -283,37 +191,37 @@ ALTER TABLE `users_election`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `election`
 --
 ALTER TABLE `election`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `election_admins`
 --
 ALTER TABLE `election_admins`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `position`
 --
 ALTER TABLE `position`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users_election`
 --
 ALTER TABLE `users_election`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
